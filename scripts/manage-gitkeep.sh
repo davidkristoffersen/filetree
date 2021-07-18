0697a9df81b2c9c98eda7140385bfecdf2d2da0b
David Kristoffersen <david.alta2010@gmail.com>
2021-07-19 00:50:40 +0200

    Parallelize gitkeep manager

--- scripts/manage-gitkeep.sh
+++ scripts/manage-gitkeep.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-set-gitkeep() {
-	for dir in $1; do
-		cd $dir
-		if [ -z "$(ls -d */ 2>/dev/null)" ] && [ -z "$(find . -maxdepth 1 -type f | grep -v ".gitkeep")" ]; then
-			touch .gitkeep
+fix-keep() {
+	cd "$1"
+	if [ ! -z "$(ls -d */ 2>/dev/null)" ] \
+		&& [ -f ".gitkeep" ]; then
+		git rm .gitkeep 2>/dev/null
+		if [ "$?" -ne "0" ]; then
+			echo "rm $(git rev-parse --show-prefix).gitkeep"
+			rm .gitkeep
 		fi
-		cd - 1>/dev/null
-	done
+	fi
+	if [ -z "$(ls -d */ 2>/dev/null)" ] \
+		&& [ -z "$(find . -maxdepth 1 -type f | grep -v ".gitkeep")" ] \
+		&& [ ! -f ".gitkeep" ]; then
+		echo "touch $(git rev-parse --show-prefix).gitkeep"
+		touch .gitkeep
+	fi
+	cd - 1>/dev/null
 }
-
-remove-gitkeep() {
-	for dir in $1; do
-		cd $dir
-		if [ ! -z "$(ls -d */ 2>/dev/null)" ]; then
-			git rm .gitkeep
-		fi
-		cd - 1>/dev/null
-	done
-}
+export -f fix-keep
 
 main() {
-	dirs="$(find . -type d -print)"
-	set-gitkeep "$dirs"
-	remove-gitkeep "$dirs"
+	find . -type d -print | parallel fix-keep
 }
 
 pushd . 1>/dev/null
