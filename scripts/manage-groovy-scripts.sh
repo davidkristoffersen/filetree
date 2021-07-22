4c3ca85b6da6b87091e52acfd31a018f39fa273b
David Kristoffersen <david.alta2010@gmail.com>
2021-07-22 16:34:18 +0200

    Fix path for groovy scripts

--- scripts/manage-groovy-scripts.sh
+++ scripts/manage-groovy-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 main() {
-    scripts_path="$WIN_HOME/AppData/Roaming/Freeplane/1.9.x/scripts/filetree"
+    scripts_path="$WIN_HOME/AppData/Roaming/Freeplane/1.9.x/scripts/"
     mkdir -p "$scripts_path"
     cp -f *.groovy "$scripts_path"
 }
